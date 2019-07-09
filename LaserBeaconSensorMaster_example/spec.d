5d228602bffe8e89395e87de37982a30778b8c54
pmx <pmx@pmx-vm18>
2019-07-09 18:38:00 +0200

    laserBeacon updates with sloeber 4.3.1

--- LaserBeaconSensorMaster_example/spec.d
+++ LaserBeaconSensorMaster_example/spec.d
@@ -1,2 +1,2 @@
 spec.o: \
- /home/pmx/Documents/sloeber_4.3.1-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
+ /home/pmx/Documents/sloeber-workspace/.metadata/.plugins/org.eclipse.cdt.managedbuilder.core/spec.C
